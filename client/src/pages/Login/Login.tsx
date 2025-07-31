8823987c7f4a4cf3895b70f6f038cc90cd04684d
Dalerbek Artikmuratov <miyuratatsu@gmail.com>
2025-07-31 13:46:09 +0500

    client setup begins

---
+++ client/src/pages/Login/Login.tsx
@@ -0,0 +1,7 @@
+const Login = () => {
+  return (
+    <div className="text-red-500">Login</div>
+  )
+}
+
+export default Login
--- server/src/routes/Category/Category.route.js
+++ server/src/routes/Category/Category.route.js
@@ -11,7 +11,6 @@ router.post(
 );
 router.get(
   '/all',
-  authMiddleware,
   CategoryValidator.getCategory(),
   expressvalidate,
   CategoryController.getCategory
